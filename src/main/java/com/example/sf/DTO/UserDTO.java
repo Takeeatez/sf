782c7fc96c64ff525f03b0c352d2325c41573abd
Takeeatez <egeg4046@naver.com>
2024-08-20 17:50:31 +0900

    fix : myPage 매핑오류 수정

--- src/main/java/com/example/sf/DTO/UserDTO.java
+++ src/main/java/com/example/sf/DTO/UserDTO.java
@@ -14,6 +14,6 @@ public class UserDTO {
     private String password;
     private String email;
     private String phone;
-
+    private String profileImage;
     // 필요에 따라 비밀번호를 포함할 수도 있지만 보통 보안을 위해 제외
 }
--- src/main/java/com/example/sf/Service/CustomUserDetailService.java
+++ src/main/java/com/example/sf/Service/CustomUserDetailService.java
@@ -2,11 +2,7 @@ package com.example.sf.Service;
 
 import com.example.sf.Entity.UserEntity;
 import com.example.sf.Repository.UserRepository;
-import jakarta.transaction.Transactional;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,32 +10,29 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@Getter
-@Setter
-@RequiredArgsConstructor
-@Transactional
 public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 데이터베이스에서 사용자 정보 조회
         UserEntity userEntity = userRepository.findByUserId(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        // UserEntity의 역할을 가져옵니다.
-        String role = "ROLE_" + userEntity.getRole().name(); // ROLE_ADMIN 또는 ROLE_USER
+        // 사용자의 권한을 설정
+        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userEntity.getRole().name()));
 
-        // Spring Security의 User 클래스를 사용하여 권한을 설정합니다.
-        return new User(
-                userEntity.getUserId(),
-                userEntity.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority(role))
-        );
+        // UserDetails 객체 반환
+        return new User(userEntity.getUserId(), userEntity.getPassword(), authorities);
     }
 }
-
-
