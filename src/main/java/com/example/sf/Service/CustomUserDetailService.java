b8227cfa25867882bb05c7bcb94e7ebf1030f6cc
Takeeatez <egeg4046@naver.com>
2024-08-22 09:02:02 +0900

    feat : Admin dashboard 생성

--- src/main/java/com/example/sf/Service/CustomUserDetailService.java
+++ src/main/java/com/example/sf/Service/CustomUserDetailService.java
@@ -2,7 +2,11 @@ package com.example.sf.Service;
 
 import com.example.sf.Entity.UserEntity;
 import com.example.sf.Repository.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,29 +14,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collections;
 
 @Service
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Transactional
 public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 데이터베이스에서 사용자 정보 조회
         UserEntity userEntity = userRepository.findByUserId(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        // 사용자의 권한을 설정
-        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userEntity.getRole().name()));
+        // UserEntity의 역할을 가져옵니다.
+        String role = "ROLE_" + userEntity.getRole().name(); // ROLE_ADMIN 또는 ROLE_USER
 
-        // UserDetails 객체 반환
-        return new User(userEntity.getUserId(), userEntity.getPassword(), authorities);
+        // Spring Security의 User 클래스를 사용하여 권한을 설정합니다.
+        return new User(
+                userEntity.getUserId(),
+                userEntity.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(role))
+        );
     }
 }
